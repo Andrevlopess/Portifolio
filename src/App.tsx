4ba069929681961a68cf6451bd729f5f99ba489d
André Lopes <andrellopes021@gmail.com>
2023-01-25 11:55:01 -0300

    framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import { Box, Button, Flex, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { Box, Button, Container, Flex, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { motion, useTransform, useScroll, useMotionValue, transform } from "framer-motion"
+import { useEffect } from 'react';
+import './Styles/Styles.css'
 
 function App() {
 
-  const {colorMode, toggleColorMode} = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode()
 
-  const teste = useColorModeValue('verdinLight', 'verdinDark')
+  const { scrollYProgress } = useScroll();
+  const x = useMotionValue(0)
+  const y = useMotionValue(0)
+  const opacity = useMotionValue(1)
+
+  useEffect(() => {
+    function updateOpacity() {
+      const maxXY = Math.max(x.get(), y.get())
+      const newOpacity = transform(maxXY, [0, 100], [1, 0])
+      opacity.set(newOpacity)
+    }
+
+    const unsubscribeX = x.on("change", updateOpacity)
+    const unsubscribeY = y.on("change", updateOpacity)
+
+    return () => {
+      unsubscribeX()
+      unsubscribeY()
+    }
+  }, [])
+
+
 
   return (
-    <Flex bgColor='uaifi' flexDirection={'column'}>
-      <Text fontSize={100} fontWeight='bold'  style={{fontFamily: "'Plus jakarta sans', sans serif"}}>
-        ai papai funcionou a
-      </Text>
-      <Button onClick={toggleColorMode}>
-        toggle
-      </Button>
-      <Flex w='100%' border='3px solid white'>
-        <Box bgColor={teste}>esse vai ser vermelho</Box>
-        <Box bgColor='verdin'>esse vai ser azul</Box>
-      </Flex>
-    </Flex>
+    <Container
+      fontWeight='bold'
+      style={{ fontFamily: "'Plus jakarta sans', sans serif" }}
+      maxW='none'
+      w='100%'>
+      <motion.div
+        initial={{ opacity: 1 }}
+      >
+        <motion.div className='progress-bar' style={{ scaleX: scrollYProgress }} />
+       <motion.div style={{ x }} />
+        <Flex h='50vh'></Flex>
+        <motion.div style={{
+          scale: scrollYProgress,
+          position: 'fixed',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
 
+        }}><Text fontSize='5em' textAlign='center'>Andre v Lopes</Text></motion.div>
+
+      </motion.div>
+      <Flex h='300vh'></Flex>
+      <Button onClick={toggleColorMode}>mudar</Button>
+    </Container>
   )
 }
 
