dbb139782582beca9a87c0da54263c01fb7298a1
André Lopes <andrellopes021@gmail.com>
2023-02-13 14:31:43 -0300

    projects responsivity

--- src/Components/Sections/ProjectsSec.tsx
+++ src/Components/Sections/ProjectsSec.tsx
@@ -1,19 +1,16 @@
 import {
     Center,
     Flex,
+    Button,
     GridItem,
     Image,
     Link,
     SimpleGrid,
     Text,
 } from "@chakra-ui/react";
-import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import TypeTodomockup from "../../Images/TypeTodoBanner.png";
-import GymNationBanner from "../../Images/GymNationBanner.png";
-import NlwSetupBanner from "../../Images/NlwSetup.png";
-import BarDoZéBanner from "../../Images/BarDoZéBanner.png";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+    faCheck,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { BsGithub } from "react-icons/bs";
 import Css from '../../Icons/Css.svg'
@@ -35,9 +32,10 @@ import GymNationMockup from '../../Icons/ProjectsMockups/GymNationMockup.svg'
 import NlwHabitsMockup from '../../Icons/ProjectsMockups/NlwHabitsMockup.svg'
 import TypeTodoMockup from '../../Icons/ProjectsMockups/TypeTodoMockup.svg'
 import BarDoZéMockup from '../../Icons/ProjectsMockups/BarDoZéMockup.svg'
+import {motion} from 'framer-motion'
 
 const ProjectsSec = () => {
- 
+
 
     interface Iproject {
         title: string;
@@ -112,32 +110,49 @@ const ProjectsSec = () => {
             >
                 {projects.map((project) => {
                     return (
-                        <Flex bgColor="bgDistinct" borderRadius='50px' p='50px' my='100px' key={project.id} w='100%' flexWrap='wrap' justifyContent='center' >
-                            <Image src={project.mockup} mb='30px' />
-                            <Flex flexDirection='column' mx='50px'>
-                                <Text fontSize='min(4em, 8.5vw)' mb='20px' textAlign='center'>
-                                    {project.title}
-                                </Text>
-                                <Text w='min(25em, 65vw)' mb='30px' fontSize='min(1.4em, 4.5vw)' >
-                                    {project.description}
-                                </Text>
-                                <Flex flexDirection='column'>
-                                    <Text color='violet100' fontSize='min(2em, 4.5vw)'>Built with:</Text>
-                                    <Flex flexWrap='wrap' w='min(30em, 65vw)' justifyContent='center' my='10px'>
+                        <motion.div initial={{y: 50, opacity: 0}} whileInView={{y: 0, opacity: 1}}  transition={{ duration: 1 }}>
+                            <Flex borderRadius='50px' p='50px' my='5vw' key={project.id} w='100%' flexWrap='wrap' justifyContent='center' >
+                                <Image src={project.mockup} mb='30px' />
+                                <Flex flexDirection='column' mx='50px'>
+                                    <Text fontSize='min(4em, 8.5vw)' mb='20px' textAlign='center'>
+                                        {project.title}
+                                    </Text>
+                                    <Text w='min(25em, 65vw)' mb='20px' fontSize='min(1.4em, 4.5vw)'>
+                                        {project.description}
+                                    </Text>
+                                    <Flex flexDirection='column'>
+                                        <Text color='violet100' fontSize='min(2em, 4.5vw)'>Built with:</Text>
+                                        <Flex flexWrap='wrap' w='min(30em, 65vw)' my='5px'>
+                                            {
+                                                project.tools?.map((tool) => {
+                                                    return (
+                                                        <Image src={tool} boxSize='min(2.5em, 6.5vw)' m='5px' _hover={{ transform: 'scale(1.4)' }} transition='.5s' />
+                                                    )
+                                                })
+                                            }
+                                        </Flex>
+
+                                    </Flex>
+                                    <Flex w='100%' my='10px'>
+                                        <Button flex={!project.url ? '1' : 'none'}>
+                                            <Link href={project.gitRepo} style={{ textDecoration: 'none' }} >
+                                                <BsGithub />
+                                            </Link>
+                                        </Button>
                                         {
-                                            project.tools?.map((tool) => {
-                                                return (
-                                                    <Image src={tool} boxSize='min(3em, 7.5vw)' m='5px' />
-                                                )
-                                            })
+                                            project.url &&
+                                            <Button p='10px' ml='20px' flex='1'>
+                                                <Link href={project.url} style={{ textDecoration: 'none' }} >
+                                                    {`Visit ${project.title}`}
+                                                </Link>
+                                            </Button>
                                         }
                                     </Flex>
 
                                 </Flex>
-
-
                             </Flex>
-                        </Flex>
+                        </motion.div>
+
                     )
                 })}
             </Flex>
